dfb32e3eae1f2e64ba60040a3144370ab8b09dab
OA Wu <comdan66@gmail.com>
2019-08-16 10:55:35 +0800

    Add Version.json

--- Pods/Target Support Files/OAKit/OAKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OAKit : NSObject
-@end
-@implementation PodsDummy_OAKit
-@end
--- Pods/Target Support Files/Pods-StopUITests/Pods-StopUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StopUITests : NSObject
-@end
-@implementation PodsDummy_Pods_StopUITests
-@end
